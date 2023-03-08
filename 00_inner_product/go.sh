514f66d67140fbe1f9b65b92a24ac5458956c6e2
NAKATA Maho <maho.nakata@gmail.com>
2023-03-08 20:33:50 +0900

    .

--- 00_inner_product/go.sh
+++ 00_inner_product/go.sh
@@ -1,8 +1,8 @@
-/usr/bin/time ./inner_product_gmp_10_naive 100000000 512
+/usr/bin/time ./inner_product_gmp_10_naive   100000000 512
 LD_LIBRARY_PATH=/home/docker/MPLAPACK/lib \
-/usr/bin/time ./inner_product_gmp_12_mpblas 100000000 512
+/usr/bin/time ./inner_product_gmp_12_mpblas  100000000 512
 
-/usr/bin/time ./inner_product_mpfr_00_naive 100000000 512
-/usr/bin/time ./inner_product_mpfr_01_fma   100000000 512
+/usr/bin/time ./inner_product_mpfr_00_naive  100000000 512
+/usr/bin/time ./inner_product_mpfr_01_fma    100000000 512
 LD_LIBRARY_PATH=/home/docker/MPLAPACK/lib \
-/usr/bin/time ./inner_product_mpfr_02_mpreal 100000000 512
+/usr/bin/time ./inner_product_mpfr_02_mpblas 100000000 512
