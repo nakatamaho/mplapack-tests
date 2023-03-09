b0a9c9320bc42ba86c6f92cdab5d351b25522ca7
NAKATA Maho <maho.nakata@gmail.com>
2023-03-09 17:48:44 +0900

    .

--- 00_inner_product/go.sh
+++ 00_inner_product/go.sh
@@ -1,5 +1,6 @@
 export LD_LIBRARY_PATH=/home/docker/MPLAPACK/lib
 /usr/bin/time ./inner_product_gmp_10_naive          100000000 512
+/usr/bin/time ./inner_product_gmp_11_openmp         100000000 512
 /usr/bin/time ./inner_product_gmp_12_mpblas         100000000 512
 /usr/bin/time ./inner_product_gmp_13_mpblas_openmp  100000000 512
 
