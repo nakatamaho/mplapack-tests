b86db92e33c70f1f509aaf643f8c6688f49e102a
NAKATA Maho <maho.nakata@gmail.com>
2023-05-10 12:23:36 +0900

    .

--- 03_gemm/go.sh
+++ 03_gemm/go.sh
@@ -7,3 +7,8 @@
 ./gemm_gmp_12_mpblas   1000 1000 1000 512 
 ./gemm_gmp_12_mpblas   1500 1500 1500 512 
 ./gemm_gmp_12_mpblas   2000 2000 2000 512 
+
+./gemm_gmp_13_mpblas_openmp    500  500  500 512 
+./gemm_gmp_13_mpblas_openmp   1000 1000 1000 512
+./gemm_gmp_13_mpblas_openmp   1500 1500 1500 512
+./gemm_gmp_13_mpblas_openmp   2000 2000 2000 512 
