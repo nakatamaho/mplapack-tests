9d157ecb4c786d335575cfa74669a4e1a08b4ac7
NAKATA Maho <maho.nakata@gmail.com>
2023-06-07 11:05:05 +0900

    .

--- 03_gemm/gemm_gmp_32_mpblaslike_blocking_openmp.cpp
+++ 03_gemm/gemm_gmp_32_mpblaslike_blocking_openmp.cpp
@@ -23,40 +23,37 @@ double flops_gemm(int k_i, int m_i, int n_i) {
     return flops;
 }
 
-#define BLOCK_X 32
-#define BLOCK_Y 32
+// Matrix multiplication kernel with blocking
+void matmul_gmp(long m, long n, long k, mpf_class alpha, mpf_class *a, long lda, mpf_class *b, long ldb, mpf_class beta, mpf_class *c, long ldc, long BLOCK_SIZE) {
+    mpf_class temp;
 
-// Blocked matrix multiplication
-void matmul_gmp_block(long i0, long j0, long m, long n, long k, mpf_class alpha, mpf_class *A, long lda, mpf_class *B, long ldb, mpf_class *C, long ldc) {
-    for (long i = i0; i < std::min(i0 + BLOCK_X, m); ++i) {
-        for (long j = j0; j < std::min(j0 + BLOCK_Y, n); ++j) {
-            mpf_class sum = 0;
-            for (long l = 0; l < k; l++) {
-                sum += A[i + l * lda] * B[l + j * ldb];
+    for (long j = 0; j < n; ++j) {
+        for (long i = 0; i < m; ++i) {
+            c[i + j * ldc] = beta * c[i + j * ldc];
+        }
+    }
+
+    for (long j = 0; j < n; j += BLOCK_SIZE) {
+        for (long l = 0; l < k; l += BLOCK_SIZE) {
+            for (long i = 0; i < m; i += BLOCK_SIZE) {
+////////////////////////////////////////////////////////////////////////////
+                for (long jj = j; jj < std::min(j + BLOCK_SIZE, n); ++jj) {
+                    for (long ll = l; ll < std::min(l + BLOCK_SIZE, k); ++ll) {
+                        temp = alpha * b[ll + jj * ldb];
+                        for (long ii = i; ii < std::min(i + BLOCK_SIZE, m); ++ii) {
+                            c[ii + jj * ldc] += temp * a[ii + ll * lda];
+                        }
+                    }
+                }
+////////////////////////////////////////////////////////////////////////////
             }
-            C[i + j * ldc] = alpha * sum + C[i + j * ldc];
-        }
-    }
-}
-
-// Matrix multiplication kernel with blocking and OpenMP parallelization
-void matmul_gmp(long m, long n, long k, mpf_class alpha, mpf_class *A, long lda, mpf_class *B, long ldb, mpf_class beta, mpf_class *C, long ldc) {
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            C[i + j * ldc] = beta * C[i + j * ldc];
-        }
-    }
-#pragma omp parallel for
-    for (long i0 = 0; i0 < m; i0 += BLOCK_X) {
-        for (long j0 = 0; j0 < n; j0 += BLOCK_Y) {
-            matmul_gmp_block(i0, j0, m, n, k, alpha, A, lda, B, ldb, C, ldc);
         }
     }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        fprintf(stderr, "Usage: %s <m> <k> <n> <prec>\n", argv[0]);
+    if (argc != 6) {
+        fprintf(stderr, "Usage: %s <m> <k> <n> <prec> <blocksize>\n", argv[0]);
         return 1;
     }
 
@@ -64,48 +61,49 @@ int main(int argc, char *argv[]) {
     int k = atoi(argv[2]);
     int n = atoi(argv[3]);
     int prec = atoi(argv[4]);
+    long block_size = atoi(argv[5]);
     mpf_set_default_prec(prec);
     int lda = k, ldb = n, ldc = n;
 
-    // Initialize and set random values for A, B, C, alpha, and beta
+    // Initialize and set random values for a, b, c, alpha, and beta
     gmp_randclass r(gmp_randinit_default);
     r.seed(42);
 
-    mpf_class *A = new mpf_class[m * k];
-    mpf_class *B = new mpf_class[k * n];
-    mpf_class *C = new mpf_class[m * n];
-    mpf_class *C_org = new mpf_class[m * n];
+    mpf_class *a = new mpf_class[m * k];
+    mpf_class *b = new mpf_class[k * n];
+    mpf_class *c = new mpf_class[m * n];
+    mpf_class *c_org = new mpf_class[m * n];
     mpf_class alpha, beta;
 
     alpha = r.get_f(prec);
     beta = r.get_f(prec);
     for (int i = 0; i < m * k; i++) {
-        A[i] = r.get_f(prec);
+        a[i] = r.get_f(prec);
     }
 
     for (int i = 0; i < k * n; i++) {
-        B[i] = r.get_f(prec);
+        b[i] = r.get_f(prec);
     }
 
     for (int i = 0; i < m * n; i++) {
-        C_org[i] = r.get_f(prec);
-        C[i] = C_org[i];
+        c_org[i] = r.get_f(prec);
+        c[i] = c_org[i];
     }
 
-    // Compute C = alpha AB + beta C \n");
+    // Compute c = alpha ab + beta c \n");
     auto start = std::chrono::high_resolution_clock::now();
-    matmul_gmp(m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+    matmul_gmp(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc, block_size);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
 
     char transa = 'n', transb = 'n';
-    Rgemm(&transa, &transb, (long)m, (long)n, (long)k, alpha, A, (long)lda, B, (long)ldb, beta, C_org, (long)ldc);
+    Rgemm(&transa, &transb, (long)m, (long)n, (long)k, alpha, a, (long)lda, b, (long)ldb, beta, c_org, (long)ldc);
 
     mpf_class tmp;
     tmp = 0.0;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            tmp += abs(C_org[i + j * ldc] - C[i + j * ldc]);
+            tmp += abs(c_org[i + j * ldc] - c[i + j * ldc]);
         }
     }
 
@@ -114,10 +112,10 @@ int main(int argc, char *argv[]) {
     gmp_printf("   %.F3e", tmp);
     printf("     %5.3f\n", elapsed_seconds.count());
 
-    delete[] A;
-    delete[] B;
-    delete[] C;
-    delete[] C_org;
+    delete[] a;
+    delete[] b;
+    delete[] c;
+    delete[] c_org;
 
     return 0;
 }
