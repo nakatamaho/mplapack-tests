c591da93ad8603d28a3a36fa2e0b41257aab3543
NAKATA Maho <maho.nakata@gmail.com>
2023-06-19 21:52:24 +0900

    .

--- 03_gemm/gemm_double_12_vectorization.cpp
+++ 03_gemm/gemm_double_12_vectorization.cpp
@@ -3,28 +3,28 @@
 #include <chrono>
 #include <stdio.h>
 #include <assert.h>
-#include <time.h>
 #include <cstring>
+#include <cpuid.h>
 
-#include "Rgemm_float.hpp"
+#include "Rgemm_double.hpp"
 
 #define MFLOPS 1e-6
 
 // cf. https://netlib.org/lapack/lawnspdf/lawn41.pdf p.120
-float flops_gemm(int k_i, int m_i, int n_i) {
-    float adds, muls, flops;
-    float k, m, n;
-    m = (float)m_i;
-    n = (float)n_i;
-    k = (float)k_i;
+double flops_gemm(int k_i, int m_i, int n_i) {
+    double adds, muls, flops;
+    double k, m, n;
+    m = (double)m_i;
+    n = (double)n_i;
+    k = (double)k_i;
     muls = m * (k + 2) * n;
     adds = m * k * n;
     flops = muls + adds;
     return flops;
 }
 
-// a vector of 256 / 32 = 8 floats
-typedef float vec __attribute__((vector_size(32)));
+// AVX2: a vector of 256 / 64 = 4 doubles
+typedef double vec __attribute__((vector_size(64)));
 
 // a helper function that allocates n vectors and initializes them with zeros
 vec *alloc(int n) {
@@ -33,51 +33,34 @@ vec *alloc(int n) {
     return ptr;
 }
 
-void matmul_float(int m, int n, int k, float alpha, float *_a, int lda, float *_b, int ldb, float beta, float *c, int ldc) {
-    if (m != n || k != n) {
-        printf("m!=n, k!=n are not supported\n");
-        exit(-1);
-    }
-    if (lda != n || ldb != n || ldc != n) {
-        printf("lda!=n, ldb!=n, ldc!=n are not supported\n");
-        exit(-1);
-    }
-    if (alpha != 1.0f) {
-        printf("alpha !=1 is supported\n");
-        exit(-1);
-    }
-    if (beta != 0.0f) {
-        printf("beta !=0 is supported\n");
-        exit(-1);
-    }
+void matmul_double(long m, long n, long k, double alpha, double *_a, long lda, double *_b, long ldb, double beta, double *c, long ldc) {
+    for (long i = 0; i < n; i++)
+        for (long j = 0; j < n; j++)
+            c[i * n + j] = beta * c[i * n + j];
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            c[i * n + j] = 0.0;
-
-    int nB = (n + 7) / 8; // number of 8-element vectors in a row (rounded up)
+    long nB = (n + 7) / 8; // number of 8-element vectors in a row (rounded up)
 
     vec *a = alloc(n * nB);
     vec *b = alloc(n * nB);
 
     // move both matrices to the aligned region
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (long i = 0; i < n; i++) {
+        for (long j = 0; j < n; j++) {
             a[i * nB + j / 8][j % 8] = _a[i * n + j];
             b[i * nB + j / 8][j % 8] = _b[j * n + i]; // <- b is still transposed
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (long i = 0; i < n; i++) {
+        for (long j = 0; j < n; j++) {
             vec s{}; // initialize the accumulator with zeros
 
             // vertical summation
-            for (int k = 0; k < nB; k++)
+            for (long k = 0; k < nB; k++)
                 s += a[i * nB + k] * b[j * nB + k];
 
             // horizontal summation
-            for (int k = 0; k < 8; k++)
+            for (long k = 0; k < 8; k++)
                 c[i * n + j] += s[k];
         }
     }
@@ -87,58 +70,69 @@ void matmul_float(int m, int n, int k, float alpha, float *_a, int lda, float *_
 }
 
 int main(int argc, char *argv[]) {
+    unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
+
+    // Get the value of the cpuid standard level 0x7
+    __get_cpuid(7, &eax, &ebx, &ecx, &edx);
+
+    if (ebx & bit_AVX2) {
+        printf("AVX2 is supported.\n");
+    } else {
+        printf("AVX2 is not supported. Exiting...\n");
+    }
+
     if (argc != 4) {
         fprintf(stderr, "Usage: %s <m> <k> <n>\n", argv[0]);
         return 1;
     }
 
-    int m = atoi(argv[1]);
-    int k = atoi(argv[2]);
-    int n = atoi(argv[3]);
-    int lda = m, ldb = k, ldc = m;
+    long m = atoi(argv[1]);
+    long k = atoi(argv[2]);
+    long n = atoi(argv[3]);
+    long lda = m, ldb = k, ldc = m;
 
     // Initialize and set random values for a, b, c, alpha, and beta
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<> random_float(0.0, 1.0);
+    std::uniform_real_distribution<> random_double(0.0, 1.0);
 
-    float *a = new float[m * k];
-    float *b = new float[k * n];
-    float *c = new float[m * n];
-    float *c_org = new float[m * n];
-    float alpha = 1.0f; // random_float(gen);
-    float beta = 0.0f;  // random_float(gen);
+    double *a = new double[m * k];
+    double *b = new double[k * n];
+    double *c = new double[m * n];
+    double *c_org = new double[m * n];
+    double alpha = 1.0f; // random_double(gen);
+    double beta = 0.0f;  // random_double(gen);
 
     for (long i = 0; i < m * k; i++) {
-        a[i] = random_float(gen);
+        a[i] = random_double(gen);
     }
     for (long i = 0; i < k * n; i++) {
-        b[i] = random_float(gen);
+        b[i] = random_double(gen);
     }
     for (long i = 0; i < m * n; i++) {
-        c_org[i] = c[i] = random_float(gen);
+        c_org[i] = c[i] = random_double(gen);
     }
 
     // compute c = alpha ab + beta c \n");
     auto start = std::chrono::high_resolution_clock::now();
-    matmul_float(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+    matmul_double(m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
     auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> elapsed_seconds = end - start;
+    std::chrono::duration<double> elapsed_seconds = end - start;
 
-    char transa = 't', transb = 't';
-    Rgemm(&transa, &transb, (long)m, (long)n, (long)k, alpha, a, (long)lda, b, (long)ldb, beta, c_org, (long)ldc);
+    char transa = 'n', transb = 'n';
+    Rgemm(&transa, &transb, m, n, k, alpha, a, lda, b, ldb, beta, c_org, ldc);
 
-    float tmp, tmp2;
+    double tmp, tmp2;
     tmp = tmp2 = 0.0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+    for (long i = 0; i < m; i++) {
+        for (long j = 0; j < n; j++) {
             tmp2 = abs(c_org[i + j * ldc] - c[j + i * ldc]);
             tmp = std::max(tmp2, tmp);
         }
     }
 
     printf("    m     n     k     MFLOPS      DIFF     Elapsed(s)\n");
-    printf("%5d %5d %5d %10.3f", m, n, k, flops_gemm(k, m, n) / elapsed_seconds.count() * MFLOPS);
+    printf("%5d %5d %5d %10.3f", (int)m, (int)n, (int)k, flops_gemm(k, m, n) / elapsed_seconds.count() * MFLOPS);
     printf("   %5.3e", tmp);
     printf("     %5.3f\n", elapsed_seconds.count());
 
